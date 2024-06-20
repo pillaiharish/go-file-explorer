3036bf37c8c78b993e66d72a626a29c2def1a243
pillaiharish <harishpillai1994@gmail.com>
2024-06-21 01:43:40 +0530

    Changed ui span for landscape browser view

--- static/script.js
+++ static/script.js
@@ -1,14 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const fileViewer = document.getElementById("file-viewer");
-
     document.querySelectorAll(".file-link").forEach(anchor => {
         anchor.addEventListener("click", function(event) {
             if (!this.target.includes("_blank")) {
                 event.preventDefault();
                 const filePath = this.getAttribute("href");
 
-                if (filePath.endsWith(".mp4") || filePath.endsWith(".flv") || filePath.endsWith(".mkv")) {
-                    fileViewer.innerHTML = `<video controls><source src="${filePath}" type="video/mp4">Your browser does not support the video tag.</video>`;
+                if (filePath.match(/\.(mp4|flv|mkv)$/)) {
+                    window.open(filePath, '_blank');
                 } else {
                     window.location.href = filePath;
                 }
